8621b23091d577f0a64a1bfe7e3df4dfeb986156
shubham tiwari <shubhamtiwari@shubhams-MacBook-Air.local>
2026-02-11 15:46:10 +0530

    ko

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import { useEffect,useState } from "react";
+import { useEffect} from "react";
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
  
